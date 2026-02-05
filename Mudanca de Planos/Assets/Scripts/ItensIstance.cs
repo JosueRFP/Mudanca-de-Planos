e298da52521df194798d0a1a66e6475c01c08efd
JosueRFP <josue.petri.senac@gmail.com>
2026-02-05 20:55:58 -0300

    O item spawna!!

--- Mudanca de Planos/Assets/Scripts/ItensIstance.cs
+++ Mudanca de Planos/Assets/Scripts/ItensIstance.cs
@@ -3,28 +3,26 @@ using UnityEngine;
 
 public class ItensIstance : MonoBehaviour
 {
-    [SerializeField] int timer = 5;
+    float timer = 15f;
     [SerializeField] GameObject[] myObjects;
 
+
+    private void Start()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
-        timer--;
-        if(timer == 0)
+        timer -= Time.deltaTime;
+        if(timer <= 0)
         {
-            timer = 5;
-            int randomIndex = Random.Range(5, myObjects.Length);
+            timer = 30;
+            print("nasceu");
+            int randomIndex = Random.Range(0, myObjects.Length);
             Vector3 randomSpawnObjPositions = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
             Instantiate(myObjects[randomIndex], randomSpawnObjPositions, Quaternion.identity);
         }
        
-    }
-
-
-
-    IEnumerator TimeToInstance()
-    {
-       
-       yield return new WaitForSeconds(timer);
     }
 }
